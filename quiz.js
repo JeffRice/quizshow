448c5b52a562543c13f6138e7839dc7b26030468
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-08-16 03:31:37 +0000

    testing rollback

--- quiz.js
+++ quiz.js
@@ -1,27 +1,41 @@
-
+var allQuestions = [];
 var correctAnswers = 0;
 var currentQuestion = 0;
-var correctPrevious = false;
-var questionjson = $.getJSON('data.json', function(data) {
 
-     //data is returned as parsed object, no need to parse it!
-     console.log(data);
-});
+var question1 = {
+    quote: "Some men just want to watch the world burn.",
+    choices: ["The Dark Knight", "The Dark Knight Rises", "Batman Begins", "Batman Returns"],
+    answer: 0
+};
 
+var question2 = {
+    quote: "Some things you see with your eyes, others you see with your heart",
+    choices: ["Fievel", "The Land Before Time", "The black Cauldron", "The Sword in the Stone"],
+    answer: 1
+};
+
+var question3 = {
+    quote: "My name is Maximus Decimus Meridius",
+    choices: ["Rome", "The Santa Clause", "Gladiator"],
+    answer: 2
+};
+
+allQuestions.push(question1);
+allQuestions.push(question2);
+allQuestions.push(question3);
 
 //dynamically add quote to the page
 $(document).ready(function() {
-        $('.quote').append(questionjson.responseJSON[currentQuestion].quote);
+        $('.quote').append(allQuestions[currentQuestion].quote);
 });
 
 
 //dynamically add choices to the page
 $(document).ready(function() {
-
 //Loop through the AllQuestions object
-    for (i = 0; i < questionjson.responseJSON[currentQuestion].choices.length; i++){
+    for (i = 0; i < allQuestions[currentQuestion].choices.length; i++){
 //append choices into the "choices" div as radio buttons
-        $('.choices').append("<input type='radio' name='movies' value=" + i + ">" + questionjson.responseJSON[currentQuestion].choices[i] + "<br/>");
+        $('.choices').append("<input type='radio' name='movies' value=" + i + ">" + allQuestions[currentQuestion].choices[i] + "<br/>");
     };
 });
 
@@ -36,28 +50,22 @@ $(document).ready(function() {
 
 		if (!isNaN(Number($('input[name=movies]:checked', '#movies').val()))){
          var answer = Number($('input[name=movies]:checked', '#movies').val());
-        if (answer === questionjson.responseJSON[currentQuestion].answer){
+        if (answer === allQuestions[currentQuestion].answer){
             correctAnswers += 1;
-            questionjson.responseJSON[currentQuestion].gotRight = true;
             $('.score').empty();
             $('.score').append(correctAnswers);
-        }
-        else {
-            questionjson.responseJSON[currentQuestion].gotRight = false;
-            $('.score').empty();
-            $('.score').append(correctAnswers);
-        }
+        };
 //remove old content
         $('.choices').empty();
         $('.quote').empty();
 //advance question
         currentQuestion += 1;
 //redraw new choices
-    for (i = 0; i < questionjson.responseJSON[currentQuestion].choices.length; i++){
-        $('.choices').append("<input type='radio' name='movies' value=" + i + ">" + questionjson.responseJSON[currentQuestion].choices[i] + "<br/>");
+    for (i = 0; i < allQuestions[currentQuestion].choices.length; i++){
+        $('.choices').append("<input type='radio' name='movies' value=" + i + ">" + allQuestions[currentQuestion].choices[i] + "<br/>");
         };
 //redraw new quote
-        $('.quote').append(questionjson.responseJSON[currentQuestion].quote);
+        $('.quote').append(allQuestions[currentQuestion].quote);
 		}
 		else {
             $('.notify').append("need to answer");
@@ -67,31 +75,6 @@ $(document).ready(function() {
 
 
 
-//return to previous question with back button
-$(document).ready(function() {
-    $('.backbutton').click(function(){
-//rewrite score; subtracting if previous answer was correct, leaving score the same if previous was inorrect
 
-        if (questionjson.responseJSON[currentQuestion - 1].gotRight){
-            correctAnswers -= 1;
-        }
-        else {
-            correctAnswers = correctAnswers;
-        }
-            $('.score').empty();
-            $('.score').append(correctAnswers);
 
-//remove old content
-        $('.choices').empty();
-        $('.quote').empty();
-//decrease question
-        currentQuestion -= 1;
-//redraw new choices
-    for (i = 0; i < questionjson.responseJSON[currentQuestion].choices.length; i++){
-        $('.choices').append("<input type='radio' name='movies' value=" + i + ">" + questionjson.responseJSON[currentQuestion].choices[i] + "<br/>");
-        }
-//redraw new quote
-        $('.quote').append(questionjson.responseJSON[currentQuestion].quote);
-        });
-});
 
